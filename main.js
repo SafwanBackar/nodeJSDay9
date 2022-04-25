7ac0fdd5ff02880b1fb05b3c02a4e21b29dff9f2
Safwan Backar <sbackar29@gmail.com>
2022-04-25 21:09:39 +0530

    day9

---
+++ main.js
@@ -0,0 +1,9 @@
+const crypto = require('crypto')
+
+var hash = crypto.createHash('md5')
+
+data = hash.update('nodejsera', 'utf-8')
+
+gen_hash = data.digest('hex');
+
+console.log('hash : ' + gen_hash)
